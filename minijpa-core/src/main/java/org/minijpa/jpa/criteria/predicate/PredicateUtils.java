a028a1957d303d3e4df5de77030d53a14464523d
Antonio Damato <adamato@Antonios-MacBook-Air.local>
2022-11-03 09:33:49 +0000

    fixed criteria parameters bug

--- minijpa-core/src/main/java/org/minijpa/jpa/criteria/predicate/PredicateUtils.java
+++ minijpa-core/src/main/java/org/minijpa/jpa/criteria/predicate/PredicateUtils.java
@@ -22,46 +22,58 @@ import java.util.Set;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Predicate;
+
 import org.minijpa.jpa.criteria.MiniParameterExpression;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PredicateUtils {
+	private static Logger LOG = LoggerFactory.getLogger(PredicateUtils.class);
 
-    private static void checkExpressionAsParameter(Expression<?> x, Set<ParameterExpression<?>> parameterExpressions) {
-	if (x != null && x instanceof MiniParameterExpression<?>)
-	    parameterExpressions.add((ParameterExpression<?>) x);
-    }
-
-    private static void findExpressions(Predicate predicate, List<Expression<?>> expressions) {
-	List<Expression<Boolean>> exprs = predicate.getExpressions();
-	if (exprs == null || exprs.isEmpty())
-	    expressions.addAll(((PredicateExpressionInfo) predicate).getSimpleExpressions());
-	else
-	    exprs.stream().filter(e -> (e instanceof Predicate)).forEachOrdered(e -> {
-		findExpressions((Predicate) e, expressions);
-	    });
-    }
-
-    public static void findExpressions(Predicate[] predicates, List<Expression<?>> expressions) {
-	for (Predicate p : predicates) {
-	    findExpressions(p, expressions);
+	private static void checkExpressionAsParameter(Expression<?> x, Set<ParameterExpression<?>> parameterExpressions) {
+		if (x != null && x instanceof MiniParameterExpression<?>)
+			parameterExpressions.add((ParameterExpression<?>) x);
 	}
-    }
 
-    public static Set<ParameterExpression<?>> findParameters(Predicate predicate) {
-	Set<ParameterExpression<?>> parameterExpressions = new HashSet<>();
-	PredicateExpressionInfo predicateExpressionInfo = (PredicateExpressionInfo) predicate;
-	predicateExpressionInfo.getSimpleExpressions().stream().forEach(e -> {
-	    checkExpressionAsParameter(e, parameterExpressions);
-	});
-
-	return parameterExpressions;
-    }
-
-    public static void findTopLevelExpressions(MultiplePredicate multiplePredicate, List<Expression<Boolean>> expressions) {
-	Predicate[] predicates = multiplePredicate.getRestrictions();
-	for (Predicate p : predicates) {
-	    if (p instanceof MultiplePredicate)
-		expressions.add(p);
+	private static void findExpressions(Predicate predicate, List<Expression<?>> expressions) {
+		List<Expression<Boolean>> exprs = predicate.getExpressions();
+		if (exprs == null || exprs.isEmpty())
+			expressions.addAll(((PredicateExpressionInfo) predicate).getSimpleExpressions());
+		else
+			exprs.stream().filter(e -> (e instanceof Predicate)).forEachOrdered(e -> {
+				findExpressions((Predicate) e, expressions);
+			});
 	}
-    }
+
+	public static void findExpressions(Predicate[] predicates, List<Expression<?>> expressions) {
+		for (Predicate p : predicates) {
+			findExpressions(p, expressions);
+		}
+	}
+
+	private static void findPredicateParameters(Predicate predicate, Set<ParameterExpression<?>> parameterExpressions) {
+		PredicateExpressionInfo predicateExpressionInfo = (PredicateExpressionInfo) predicate;
+		for (Expression<?> expression : predicateExpressionInfo.getSimpleExpressions()) {
+			if (expression instanceof Predicate)
+				findPredicateParameters(((Predicate) expression), parameterExpressions);
+			else
+				checkExpressionAsParameter(expression, parameterExpressions);
+		}
+	}
+
+	public static Set<ParameterExpression<?>> findParameters(Predicate predicate) {
+		Set<ParameterExpression<?>> parameterExpressions = new HashSet<>();
+		PredicateExpressionInfo predicateExpressionInfo = (PredicateExpressionInfo) predicate;
+		findPredicateParameters(predicate, parameterExpressions);
+		return parameterExpressions;
+	}
+
+	public static void findTopLevelExpressions(MultiplePredicate multiplePredicate,
+			List<Expression<Boolean>> expressions) {
+		Predicate[] predicates = multiplePredicate.getRestrictions();
+		for (Predicate p : predicates) {
+			if (p instanceof MultiplePredicate)
+				expressions.add(p);
+		}
+	}
 }
